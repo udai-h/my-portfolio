529146c99fcc749fdf61c1f5957db123e900e59b
Yudai Hinata <z5380940@ad.unsw.edu.au>
2025-05-30 13:44:54 +1000

    layout fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MobileHeader from "./components/MobileHeader";
 import About from "./components/About";
 import "./index.css";
 import Education from "./components/Education";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
@@ -11,12 +12,13 @@ export default function App() {
       <div className="block lg:hidden p-6">
         <MobileHeader />
       </div>
-      <aside className="hidden lg:block lg:w-2/5 p-10 pl-36 h-full min-h-screen">
+      <aside className="hidden lg:block lg:w-2/5 p-10 pl-36 h-screen sticky top-0">
         <Header />
       </aside>
       <main className="w-full lg:w-3/5 overflow-y-auto px-6 md:pl-10 md:pr-36 py-10">
         <About />
         <Education />
+        <Skills />
       </main>
     </div>
   );
---
+++ src/components/Skills.tsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <section className="max-w-3xl mx-auto px-6 lg:py-24 text-left lg:text-lg text-base">
+      <h2 className="text-xl font-bold mb-4 lg:hidden">About</h2>
+      <p className="text-navy dark:text-beige">
+        I'm a software engineer studying Computer Science at The University of
+        New South Wales.
+      </p>
+      <p className="text-navy dark:text-beige mb-4">
+        My life goal is to create new value in the world through software
+        development.
+      </p>
+      <p className="text-navy dark:text-beige mb-4">
+        With an academic background in Human-Computer Interaction and
+        Installation Art, I strive to build software that people can truly feel
+        and interact with.
+      </p>
+      <p className="text-navy dark:text-beige mb-4">
+        I'm especially interested in both web and mobile app development, and I
+        aspire to contribute to services from both frontend and backend
+        perspectives.
+      </p>
+      <p className="text-navy dark:text-beige">
+        In addition, through my involvement in an international non-profit
+        organization, I've developed strong communication and leadership skills,
+        which I bring to both collaborative and cross-cultural projects.
+      </p>
+    </section>
+  );
+}
